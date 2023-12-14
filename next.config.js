af94499cf69da3fd894e849fc0bdad9ee9be186f
Joseph Tran <100446142+jknt27@users.noreply.github.com>
2023-12-14 13:56:43 -0800

    Update GH Deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/xc-website' : '',
     output: 'export',
     distDir: 'dist',
     images: {
